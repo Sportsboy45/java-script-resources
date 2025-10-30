165ab32b5c37ec036795a8674bcdfe3f451fc4d2
Rahul Kumar <145127071+Sportsboy45@users.noreply.github.com>
2025-10-30 11:37:15 +0000

    JAVA SCRIPT RESOURCES

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log("Rahul")
+const accountId = 1258636
+var accountEmail= "rahul98764@gmail.com"
+let accountPassword ="428673595863"
+accountCity="Noida"
+let rahul 
+let hukle=null
+console.table([accountId,accountEmail,accountPassword,accountCity,rahul,hukle])
