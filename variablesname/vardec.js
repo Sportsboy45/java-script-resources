b8ba89aef7471552dd7b57c98afc92c13c8964ee
Rahul Kumar <145127071+Sportsboy45@users.noreply.github.com>
2025-10-30 11:53:25 +0000

    variablesname

---
+++ variablesname/vardec.js
@@ -0,0 +1,8 @@
+const a=20
+var b=30
+let c= 40
+//const variable is unchangable h
+var b=50 // var is multiple time defined and update the value
+
+// let c=60  let variable is not defined 
+console.table([a,b,c])
