fd251a4b23d1fabd74f2271f1068e31f2736513b
Rahul Kumar <145127071+Sportsboy45@users.noreply.github.com>
2025-10-30 11:57:45 +0000

    variablesname

--- variablesname/vardec.js
+++ variablesname/vardec.js
@@ -1,8 +1,8 @@
 const a=20
 var b=30
 let c= 40
-//const variable is unchangable h
-var b=50 // var is multiple time defined and update the value
+//const variable is unchangable h and block scoped
+var b=50 // var is multiple time defined and update the value and function scoped, globle scoped, reassined 
 
 // let c=60  let variable is not defined 
 console.table([a,b,c])
